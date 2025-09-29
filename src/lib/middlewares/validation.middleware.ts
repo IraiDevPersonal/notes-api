06b9c5eb0f0025b6b37837cfa669c72c83314040
IraiDev <dev.iarriagada@gmail.com>
2025-09-29 11:59:20 -0300

    add: se agrego middleware de validacion de request y esquema para params id

---
+++ src/lib/middlewares/validation.middleware.ts
@@ -0,0 +1,46 @@
+import type { NextFunction, Request, Response } from "express";
+import type z from "zod";
+import { ResponseController } from "../controllers/response.controller";
+import { SchemaErrorHandler } from "../errors/schema-error-handler";
+import { logger } from "../logger";
+
+export class ValidationMiddleware {
+	static validateRequest = (schemas: {
+		body?: z.ZodSchema;
+		params?: z.ZodSchema;
+		query?: z.ZodSchema;
+	}) => {
+		return (req: Request, res: Response, next: NextFunction): void => {
+			try {
+				if (schemas.body) {
+					req.body = schemas.body.parse(req.body);
+				}
+
+				if (schemas.params) {
+					schemas.params.parse(req.params);
+				}
+
+				if (schemas.query) {
+					schemas.query.parse(req.query);
+				}
+
+				next();
+			} catch (error) {
+				const responseController = new ResponseController(res);
+
+				if (SchemaErrorHandler.isSchemaError(error)) {
+					const errorMessage = SchemaErrorHandler.getErrorMessage(error);
+					logger.error({
+						source: "ValidationMiddleware/validateRequest",
+						message: errorMessage,
+						error,
+					});
+					responseController.error(errorMessage, 400);
+					return;
+				}
+
+				next(error);
+			}
+		};
+	};
+}
